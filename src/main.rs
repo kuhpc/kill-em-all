18fc73a984ec166fe728b79eea08a0586447f522
Shirshak <8097377+shirshak55@users.noreply.github.com>
2019-12-16 22:32:49 +0545

    Init Commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!(
+        "{:>5} {:^5} {:>8} {:>8} {:.100}",
+        "PID", "STATE", "UTIME", "STIME", "CMD"
+    );
+
+    for p in &psutil::process::all().unwrap() {
+        println!(
+            "{:>5} {:^5} {:>8.2} {:>8.2} {:.100}",
+            p.pid,
+            p.state.to_string(),
+            p.utime,
+            p.stime,
+            p.cmdline()
+                .unwrap()
+                .unwrap_or_else(|| format!("[{}]", p.comm))
+        );
+    }
+}
