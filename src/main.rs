8dd1466ece0fc5b5e396edafb0e733153700d0df
Shirshak <8097377+shirshak55@users.noreply.github.com>
2020-01-24 14:46:36 +0000

    Add more comments

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,13 @@ fn main() {
                 let process_name = &process.name().unwrap_or("Unnamed".to_owned());
                 let process_id = &process.uids().expect("Unable to get Id").real;
 
+                // For testing run stress which use cpu too much.
                 if let Some(_) = process_name.find("stress") {
                     dbg!(current_time, interval, process_name, process_id);
                 }
 
+                // Todo Make this so that it kills only user related process not root, admin related stuff.
+                // Needs to be efficient
                 if *cpu_usage > 5.0 && interval > 5 * 60 {
                     if let Ok(process_to_kill) = psutil::process::Process::new(process.pid()) {
                         println!("Killing Process {}", process_name);
